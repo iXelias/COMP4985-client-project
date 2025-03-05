90bd930e9ab2758dc0c935f71161bf53ccb5c5a5
xelias <cyberandpower@gmail.com>
2025-03-05 09:18:01 -0800

    Temporary push

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <../include/account.h>
-#include <../include/connection.h>
+#include "../include/account.h"
+#include "../include/connection.h"
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,23 +12,51 @@
 // #define LOGOUT 0x0C
 #define CREATE_ACCOUNT 0x0D
 
-int main(void)
+struct networkSocket
 {
-    int server_sock_fd;
-    server_sock_fd = connect_to_server();
+    int                client_fd;
+    struct sockaddr_in address;
+};
+
+int main(int argc, char *argv[])
+{
+    struct networkSocket data;
+    char                 detected_ip[INET_ADDRSTRLEN];
+    char                *manual_address;
+    in_port_t            manual_port;
+    data.client_fd = 0;
+
+    manual_address = NULL;
+    manual_port    = 0;
+
+    parse_args(argc, argv, &manual_address, &manual_port);
+
+    if(manual_address == NULL)
+    {
+        in_addr_t ip;
+        find_address(&ip, detected_ip);
+        manual_address = detected_ip;
+    }
+
+    data.client_fd = setup_client(&data.address, manual_address, manual_port);
+    if(data.client_fd < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    display_port(&data.address, manual_address);
 
     // Create account
-    account_request(server_sock_fd, TESTUSERNAME, TESTPASSWORD, CREATE_ACCOUNT);
-    account_response(server_sock_fd);
+    account_request(data.client_fd, TESTUSERNAME, TESTPASSWORD, CREATE_ACCOUNT);
+    account_response(data.client_fd);
 
     // Login account
-    account_request(server_sock_fd, TESTUSERNAME, TESTPASSWORD, LOGIN);
-    account_response(server_sock_fd);
+    account_request(data.client_fd, TESTUSERNAME, TESTPASSWORD, LOGIN);
+    account_response(data.client_fd);
 
     // Logout accont
-    account_logout(server_sock_fd);
-    account_response(server_sock_fd);
+    account_logout(data.client_fd);
+    account_response(data.client_fd);
 
-    close(server_sock_fd);
+    close(data.client_fd);
     return EXIT_SUCCESS;
 }
